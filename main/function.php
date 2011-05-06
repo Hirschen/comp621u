8241d9d617615f5dca98186d1e60a93fbdd1f2e6
cedric waldburger <cedric@asmallworld.net>
2011-05-06 23:25:20 +0800

    added 2nd function, corrected problem that limited results to 100

--- main/function.php
+++ main/function.php
@@ -3,7 +3,7 @@ function fetch_data_by_date($date) {
 	try
 	{
 		$db=new PDO('sqlite:post_coordinates.db');
-		$result=$db->query("select * from post_coordinates where POST_CREATED_AT between '".$date." 00:00:00' and '".$date." 23:59:59.999999' limit 100");
+		$result=$db->query("select * from post_coordinates where POST_CREATED_AT between '".$date." 00:00:00' and '".$date." 23:59:59.999999'");
 		$db = NULL; // close db connection
 		$array = $result->fetchAll(PDO::FETCH_ASSOC);
 		unset($result);
@@ -22,6 +22,28 @@ function fetch_data_by_date($date) {
 }
 
 fetch_data_by_date($_GET["date"]);
+
+function get_thread_nr($startdate, $enddate) {
+	try
+	{
+		$db=new PDO('sqlite:post_coordinates.db');
+		$result=$db->query("select count(*) from post_coordinates where POST_CREATED_AT between '".$startdate." 00:00:00' and '".$enddate." 23:59:59.999999'");
+		$db = NULL; // close db connection
+		$array = $result->fetchAll(PDO::FETCH_ASSOC);
+		unset($result);
+		$json = json_encode($array);
+		// $myFile = "testFile.txt";
+		// $fh = fopen($myFile, 'w') or die("can't open file");
+		// fwrite($fh, $json);
+		// fwrite($fh, $date);
+		// fclose($fh);
+		echo $json;
+	}
+	catch(PDOException $e) {
+		print 'Exception : '.$e->getMessage();
+	}
+
+}
 
 ?>
 
